1ac5a9813b583f2d54735e8d64a50135e1fc90df
Rence Salaveria <cbsalaveria@gmail.com>
2024-02-10 11:10:43 +0800

    perf(Update Task): Add `sometimes` flag to EditTaskRequest
    Include `sometimes` in the array so the method would work as expected with PATCH or PUT requests

--- app/Http/Requests/EditTaskRequest.php
+++ app/Http/Requests/EditTaskRequest.php
@@ -12,13 +12,16 @@ class EditTaskRequest extends FormRequest
     public function rules(): array
     {
         return [
-            'task_name' => ['required'],
-            'task_description' => ['required'],
+            'task_name' => ['sometimes', 'required'],
+            'task_description' => ['sometimes', 'required'],
             'task_priority' => [
+                'sometimes',
                 'required',
                 new Enum(TaskPriority::class),
             ],
-            'task_status' => ['required',
+            'task_status' => [
+                'sometimes',
+                'required',
                 new Enum(TaskStatus::class),
             ],
         ];
